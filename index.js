245f133b14ea5b8661f75c26d8f95c2b995d4a35
blop <morgan.grt@outlook.fr>
2021-02-22 00:29:11 +0100

    update repo

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const port = 80;
 
-const server = http.createServer((req, res) => {
+const options = {
+      key: fs.readFileSync('selfsigned.key'),
+      cert: fs.readFileSync('selfsigned.crt')};
+
+const server = http.createServer(options, (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
