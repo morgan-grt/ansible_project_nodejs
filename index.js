e4926c4148524cd2eac4ac6ac7b9222e9c2b0ea5
blop <morgan.grt@outlook.fr>
2021-02-22 15:59:21 +0100

    update repo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const https = require('https');
 const http = require('http');
 const fs = require('fs');
 const express = require('express');
+const greenlock = require('greenlock-express');
 
 const hostname = '0.0.0.0';
 const port_https = 443;
@@ -9,15 +10,28 @@ const port_http = 80;
 
 const app = express();
 
-const options = {
-      key: fs.readFileSync('/var/www/html/selfsigned.key'),
-      cert: fs.readFileSync('/var/www/html/selfsigned.crt')};
-
 app.use((req, res) => {
 	res.statusCode = 200;
   	res.setHeader('Content-Type', 'text/plain');
 	res.send('🚀 Hello there !');
 });
+
+greenlock.init({
+	packageRoot: __dirname,
+    configDir: "./greenlock.d",
+
+    // contact for security and critical bug notices
+    maintainerEmail: "none@none.com",
+
+    // whether or not to run at cloudscale
+    cluster: false
+}).serve(app);
+
+/*const options = {
+      key: fs.readFileSync('/var/www/html/selfsigned.key'),
+      cert: fs.readFileSync('/var/www/html/selfsigned.crt')};
+
+
 
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(options, app);
@@ -29,3 +43,4 @@ httpServer.listen(port_http, hostname, () => {
 httpsServer.listen(port_https, hostname, () => {
 	console.log('🚀 HTTPS Server running on port 443');
 });
+*/
