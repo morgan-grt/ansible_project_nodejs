8ac36554240a09f0edd97bc9c6531467912c56de
blop <morgan.grt@outlook.fr>
2021-02-22 01:10:52 +0100

    update repo

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const http = require('https');
+const https = require('https');
+const http = require('http');
 const fs = require('fs');
+const express = require('express');
 
 const hostname = '0.0.0.0';
-const port = 3000;
+const port_https = 443;
+const port_http = 80;
+
+const app = express();
 
 const options = {
       key: fs.readFileSync('selfsigned.key'),
       cert: fs.readFileSync('selfsigned.crt')};
 
-const server = http.createServer(options, (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.use((req, res) => {
+	res.statusCode = 200;
+  	res.setHeader('Content-Type', 'text/plain');
+	res.send('Hello there !');
+});
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(options, app);
+
+httpServer.listen(port_http, hostname, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(port_https, hostname, () => {
+	console.log('HTTPS Server running on port 443');
 });
 
 server.listen(port, hostname, () => {
