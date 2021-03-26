9c75e1532779f246d6b47ddf3de7cf3d3a41ef29
blop <morgan.grt@outlook.fr>
2021-03-26 11:33:51 +0100

    update repo

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const https = require('https');
 const http = require('http');
 const fs = require('fs');
 const express = require('express');
 
 const hostname = '0.0.0.0';
-const port_https = 443;
 const port_http = 80;
 
 const app = express();
@@ -12,21 +10,12 @@ const app = express();
 app.use((req, res) => {
 	res.statusCode = 200;
   	res.setHeader('Content-Type', 'text/plain');
-	res.send('🚀 Hello there !');
+	res.send('🚀 Bonjour projet annuel !');
 });
 
-const options = {
-      key: fs.readFileSync('/var/www/html/certs/key.pem'),
-      cert: fs.readFileSync('/var/www/html/certs/cert.pem'),
-      passphrase: 'blop'};
-
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(options, app);
 
 httpServer.listen(port_http, hostname, () => {
 	console.log('🚀 HTTP Server running on port 80');
 });
 
-httpsServer.listen(port_https, hostname, () => {
-	console.log('🚀 HTTPS Server running on port 443');
-});
